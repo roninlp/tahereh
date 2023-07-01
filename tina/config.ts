a63216da4a54f28e621555525660b1d14d024479
roninlp <ronin.lp@gmail.com>
2023-07-01 12:50:51 +0330

    ignore error

--- tina/config.ts
+++ tina/config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-
 export default defineConfig({
   branch,
+  //@ts-ignore
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+  //@ts-ignore
   token: process.env.NEXT_PUBLIC_TINA_TOKEN_ID, // Get this from tina.io
 
   admin: {
