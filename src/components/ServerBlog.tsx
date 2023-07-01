37e551b99ff9c2173fe25f5b3177ebe89303e3f3
roninlp <ronin.lp@gmail.com>
2023-07-01 12:43:51 +0330

    remove old files

--- src/components/ServerBlog.tsx
+++ src/components/ServerBlog.tsx
@@ -1,5 +1,4 @@
 import { PostQuery } from "@/../tina/__generated__/types";
-import { useTina } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 interface Props {
