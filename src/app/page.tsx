b8838f6ebadadb0d16a2236b91e674eea37641cb
roninlp <ronin.lp@gmail.com>
2023-08-01 22:51:07 +0330

    force dynamic added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: "طاهره محزون",
   description: "نویسنده محتوا",
 };
-export const revalidate = 60;
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   return (
