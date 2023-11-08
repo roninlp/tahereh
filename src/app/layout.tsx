a1f64fee6a52c97ccadbdc5f0a5a70244d38f7b1
A.Fahim <ali.fahim92@outlook.com>
2023-11-08 13:48:05 +0330

    added clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "@/app/globals.css";
 import Header from "@/components/Header";
 import { Paeez, Vazir } from "@/fonts/fonts";
+import Script from 'next/script'
+
 
 export default function RootLayout({
   children,
@@ -9,6 +11,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${Vazir.variable} ${Paeez.variable}`}>
+      <head>
+      <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "jn8ct6hfb6");
+      </script>
+      </head>
       <body
         className={
           process.env.NODE_ENV === "development" ? "debug-screens" : ""
